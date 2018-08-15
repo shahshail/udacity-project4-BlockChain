a5fb746dee56cd9207608ed45ad9f9b6b89eb89e
Shail Shah <shahshail9898@gmail.com>
2018-08-15 00:37:17 -0400

    Block height and time-stamp

--- simpleChain.js
+++ simpleChain.js
@@ -21,11 +21,14 @@ class Block{
 class Blockchain{
     constructor(){
         this.chain = [];
+        this.addBlock(new Block("First Block in the chain - Genesis block"));
     }
 
     addBlock(newBlock){
+        newBlock.heigt = this.chain.length
+        newBlock.time = new Date().getTime().toString().slice(0,-3);
         if(this.chain.length > 0){
-            newBlock.previousblockhash = this.chain[this.chain.length-1].hash
+            newBlock.previousblockhash = this.chain[this.chain.length-1].hash;
         }
         newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
         this.chain.push(newBlock);
