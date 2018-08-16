648e856866c80fc868cdb996ff05263060dbe29c
Shail Shah <shahshail9898@gmail.com>
2018-08-16 19:54:16 -0400

    levelDB Add and Get operation

--- levelSandBox.js
+++ levelSandBox.js
@@ -3,3 +3,18 @@
 let level = require('level');
 let chainDB = './chaindata';
 let db = level(chainDB);
+
+//Add data with levelDB with key-value pair
+function addLevelDBData(key, value){
+    db.put(key,value, function(err) {
+        if(err) return console.log('Block ' + key + ' submition Failed', err)
+    })
+}
+
+//Get data from LevelDB with key/value pair
+function getLevelDBData(key){
+    db.get(key, function(err, value){
+        if(err) return console.log('Not Found!', err);
+        console.log('Value = ' + value);
+    })
+}
